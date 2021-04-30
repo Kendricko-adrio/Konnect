be00d2eab8f736e785d093d59325ce6147681730
Kendricko-adrio <kendrickoadrio134@gmail.com>
2021-04-30 13:45:57 +0700

    My First Commit

---
+++ app/src/main/java/edu/bluejack20_2/Konnect/MainActivity.kt
@@ -0,0 +1,20 @@
+package edu.bluejack20_2.Konnect
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+// Write a message to the database
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+    }
+}
