2e0ef14d4b7da267c47034238dc37d70d2474153
Kendricko-adrio <kendrickoadrio134@gmail.com>
2021-05-02 01:20:34 +0700

    Login sudah bisa jalan

---
+++ app/src/main/java/edu/bluejack20_2/Konnect/HomeActivity.kt
@@ -0,0 +1,23 @@
+package edu.bluejack20_2.Konnect
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var btnLogOut: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        btnLogOut = findViewById(R.id.btnSignOut)
+        btnLogOut.setOnClickListener(View.OnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+        })
+    }
+}
